b60296f8a8acce62ae209097bbad28417cab126c
jinfanghuang <2283785225@qq.com>
2024-12-17 14:56:19 +0800

    feat: 添加 ConfigProvider

--- src/layout.js
+++ src/layout.js
@@ -1,33 +1,46 @@
 import {Outlet, useNavigate} from "react-router-dom";
-import logo from './asset/logo.png';
+import {ConfigProvider} from 'antd';
 
 const Layout = ({navigation}) => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <div className="header">
-        <div className="logo">
-          {/*<img src={logo} />*/}
+    <ConfigProvider
+      theme={{
+        token: {
+          // Seed Token
+          colorPrimary: '#FFAA20',
+          borderRadius: 2,
+
+          // Alias Token
+          colorBgContainer: '#f6ffed',
+        },
+      }}
+    >
+      <div>
+        <div className="header">
+          <div className="logo">
+            {/*<img src={logo} />*/}
+          </div>
+          {
+            navigation.map(({label, url}) => (
+              <div
+                className={`header-link ${window.location.href.includes(url) ? 'active' : ''}`}
+                key={url}
+                onClick={() => {
+                  navigate(url);
+                }}
+              >
+                {label}
+              </div>
+            ))
+          }
         </div>
-        {
-          navigation.map(({label, url}) => (
-            <div
-              className={`header-link ${window.location.href.includes(url) ? 'active' : ''}`}
-              key={url}
-              onClick={() => {
-                navigate(url);
-              }}
-            >
-              {label}
-            </div>
-          ))
-        }
+        <div className='layout-inner'>
+          <Outlet />
+        </div>
       </div>
-      <div className='layout-inner'>
-        <Outlet />
-      </div>
-    </div>
+    </ConfigProvider>
   )
 };
 
